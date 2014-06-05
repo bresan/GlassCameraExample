5043f286948eaef3a50589dfa4a084115a0f2171
Rodrigo Bresan <rcbresan@gmail.com>
2014-06-05 16:48:57 -0300

    Fixed some permissions on AndroidManifest.xml

--- src/com/bresan/glass/examples/CameraExample/CameraActivity.java
+++ src/com/bresan/glass/examples/CameraExample/CameraActivity.java
@@ -1,26 +1,33 @@
 package com.bresan.glass.examples.CameraExample;
 
-//Special thanks to Nortom Lam for Camera manipulation example in Android
-
-import java.io.IOException;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.PixelFormat;
 import android.hardware.Camera;
+import android.media.AudioManager; // For option 2
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.KeyEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class CameraActivity extends Activity {
+import com.google.android.glass.media.Sounds; // For option 2
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CameraActivity extends Activity {
 	private SurfaceHolder surfaceHolder;
 	private Camera camera;
 	private boolean previewOn;
-	private final static int FPS_LIMIT = 5000;
 
 	Context context = this;
 
@@ -30,6 +37,7 @@ public class CameraActivity extends Activity {
 
 		setContentView(R.layout.camera_preview);
 
+		// Set up the camera preview UX
 		getWindow().setFormat(PixelFormat.UNKNOWN);
 		SurfaceView surfaceView = (SurfaceView) findViewById(R.id.camerapreview);
 		surfaceHolder = surfaceView.getHolder();
@@ -39,23 +47,39 @@ public class CameraActivity extends Activity {
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		switch (keyCode) {
+		// Hardware camera button
 		case KeyEvent.KEYCODE_CAMERA: {
+
+			// Release the camera before the picture is taken
 			camera.stopPreview();
 			camera.release();
 			previewOn = false;
 
+			// Return false to allow the camera button to do its default action
 			return false;
 		}
-
+		// Touchpad tap
 		case KeyEvent.KEYCODE_DPAD_CENTER: // Alternative way to take a picture
 		case KeyEvent.KEYCODE_ENTER: {
+			android.util.Log.d("CameraActivity", "Tap.");
 
+			// Option 1: release the camera and use the ACTION_IMAGE_CAPTURE intent
 			camera.stopPreview();
 			camera.release();
 			previewOn = false; // Don't release the camera in surfaceDestroyed()
 
+			// Use the image capture intent to take the picture and process it with onActivityResult()
 			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 			startActivityForResult(intent, 0);
+
+			// Option 2: capture the picture yourself and process it with a Camera.PictureCallback
+
+			// Play a sound to indicate the take picture action succeeded
+			// AudioManager audio = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+			// audio.playSoundEffect(Sounds.SUCCESS);
+
+			// Take the picture
+			// camera.takePicture(null, null, new SavePicture());
 
 			return true;
 		}
@@ -83,7 +107,7 @@ public class CameraActivity extends Activity {
 				try {
 					// This camera parameter is set to fix a bug in XE12 that garbles the preview
 					Camera.Parameters params = camera.getParameters();
-					params.setPreviewFpsRange(FPS_LIMIT, FPS_LIMIT);
+					params.setPreviewFpsRange(5000, 5000);
 					camera.setParameters(params);
 
 					// Start the preview
